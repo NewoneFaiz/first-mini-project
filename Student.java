554b91c02e7ea3197be6a53235c14ce475973d7d
newonefaiz <mohamedfaisalv3@gmail.com>
2025-11-06 20:03:08 +0530

    Student.java file added

--- Student.java
+++ Student.java
@@ -1,29 +1,19 @@
 public class Student {
     private String name;
     private int marks;
-
-    // Constructor
     public Student(String name, int marks) {
         this.name = name;
         this.marks = marks;
     }
-
-    // Getter for name
     public String getName() {
         return name;
     }
-
-    // Setter for name
     public void setName(String name) {
         this.name = name;
     }
-
-    // Getter for marks
     public int getMarks() {
         return marks;
     }
-
-    // Setter for marks
     public void setMarks(int marks) {
         this.marks = marks;
     }
--- mini.java
+++
@@ -1,4 +0,0 @@
-public class mini {
-    public static void main(String[]args){
-    String f1= "faisal";
-}
