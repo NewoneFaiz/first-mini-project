a10501660f23818dbf5420836428c3da4f3f507f
newonefaiz <mohamedfaisalv3@gmail.com>
2025-11-06 17:36:50 +0530

    complete

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+ public static void main(string[]args){
+    string f1= "faisal";
+ }
+}
---
+++ mini.java
@@ -0,0 +1,4 @@
+public class mini {
+    public static void main(String[]args){
+    String f1= "faisal";
+}
